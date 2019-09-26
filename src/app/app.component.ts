4d589bc3815f6e11695272d8238f72a17ae28bd3
sschoor163 <steven_Schoor@yahoo.com>
2019-09-25 20:56:48 -0500

    Todo list assignment 9/25/19 Steven Schoor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todos';
+  todoList: any [] = [];
+  todoTitle: string;
+
+  ngOnInit(){
+    this.todoTitle='';
+    this.todoList = [
+      {title: 'Install Angular CLI', isDone: false},
+    ];
+  }
+addTodo():void{
+  this.todoList.push({
+    title:this.todoTitle,
+    isDone:false
+  });
+  this.todoTitle="";
 }
+
+deleteTodo(todo:any) {
+  const index = this.todoList.findIndex(todoItem=> todoItem==todo);
+  this.todoList.splice(index,1);
+
+}
+
+}
+
